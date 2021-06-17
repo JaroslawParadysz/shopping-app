29751e18ac46ec80c176ac2a0739fdf0b504a510
Jaroslaw Paradysz <jaroslaw.a.paradysz@capgemini.com>
2021-06-17 21:09:14 +0200

    Refactored tests

--- ShoppingApp/Tests/ShoppingApp.Modules.Products.Integration.Tests/ProductsTests.cs
+++ ShoppingApp/Tests/ShoppingApp.Modules.Products.Integration.Tests/ProductsTests.cs
@@ -11,39 +11,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace ShoppingApp.Modules.Products.Integration.Tests
 {
-    public class ServicesFixture
+    public class ServicesFixture : IDisposable
     {
+        public IServiceCollection ServiceCollection { get; set; }
+
         public ServicesFixture()
         {
-            var cat = new Core.DomainModels.Category
-            {
-                Name = "Abc Category",
-                Products = new List<Product> { new Product { Name = "Abc Product" } }
-            };
-
             RegisterServicesAction = (svc) => {
                 var serviceDescriptor = svc.SingleOrDefault(
                     d => d.ServiceType ==
                         typeof(DbContextOptions<ProductsDbContext>));
                 svc.Remove(serviceDescriptor);
                 svc.AddDbContext<ProductsDbContext>(opt => opt.UseInMemoryDatabase(databaseName: "InMemoryDb"));
-
-                var sp = svc.BuildServiceProvider();
-                var dbContext = sp.GetService<ProductsDbContext>();
-                dbContext.Categories.Add(cat);
-                dbContext.SaveChanges();
+                ServiceCollection = svc;
             };
         }
 
         public Action<IServiceCollection> RegisterServicesAction { get; set; }
+
+        public void Dispose()
+        {
+            int i = 10;
+        }
     }
 
-    public class ProductsTests : IClassFixture<ServicesFixture>
+    public class ProductsTests : IClassFixture<ServicesFixture>, IDisposable
     {
         public ProductsTests(ServicesFixture servicesFixture)
         {
@@ -58,6 +56,19 @@ namespace ShoppingApp.Modules.Products.Integration.Tests
             var factory = new CustomWebApplicationFactory<Bootstraper.Startup>(ServicesFixture.RegisterServicesAction);
             var client = factory.CreateClient();
 
+            Category category = new Category
+            {
+                Name = "Abc Category",
+                Products = new List<Product> { new Product { Name = "Abc Product" } }
+            };
+
+            using (ServiceProvider serviceProvider = ServicesFixture.ServiceCollection.BuildServiceProvider())
+            {
+                var productDbContext = serviceProvider.GetRequiredService<ProductsDbContext>();
+                productDbContext.Categories.Add(category);
+                productDbContext.SaveChanges();
+            }
+
             var response = await client.GetAsync(ProductsRest.ProductsPath);
             response.StatusCode.Should().Be(HttpStatusCode.OK);
 
@@ -67,11 +78,24 @@ namespace ShoppingApp.Modules.Products.Integration.Tests
         }
 
         [Fact]
-        public async Task Get_Expect_BadRequest()
+        public async Task Get_For_Unexisting_ProductId_Expects_BadRequest()
         {
             var factory = new CustomWebApplicationFactory<Bootstraper.Startup>(ServicesFixture.RegisterServicesAction);
-
             var client = factory.CreateClient();
+
+            Category category = new Category
+            {
+                Name = "Abc Category",
+                Products = new List<Product> { new Product { Name = "Abc Product" } }
+            };
+
+            using (ServiceProvider serviceProvider = ServicesFixture.ServiceCollection.BuildServiceProvider())
+            {
+                var productDbContext = serviceProvider.GetRequiredService<ProductsDbContext>();
+                productDbContext.Categories.Add(category);
+                productDbContext.SaveChanges();
+            }
+
             var unexistingProductId = Guid.NewGuid();
             var response = await client.GetAsync(ProductsRest.ProductsPath + "/" + unexistingProductId);
             response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
@@ -79,6 +103,44 @@ namespace ShoppingApp.Modules.Products.Integration.Tests
             var payload = JsonConvert.DeserializeObject<Error>(await response.Content.ReadAsStringAsync());
             payload.Code.Should().Be("product_not_found");
             payload.Message.Should().Be($"Product with id {unexistingProductId} has not been found.");
+        }
+
+        [Fact]
+        public async Task Create_Expects_Success()
+        {
+            var factory = new CustomWebApplicationFactory<Bootstraper.Startup>(ServicesFixture.RegisterServicesAction);
+            var client = factory.CreateClient();
+
+            Category category = new Category
+            {
+                Name = "Abc Category"
+            };
+
+            using (ServiceProvider serviceProvider = ServicesFixture.ServiceCollection.BuildServiceProvider())
+            {
+                var productDbContext = serviceProvider.GetRequiredService<ProductsDbContext>();
+                productDbContext.Categories.Add(category);
+                productDbContext.SaveChanges();
+            }
+
+            CreateProductCommand command = new CreateProductCommand { CategoryId = category.Id, ProductName = "Probuct Abc" };
+            var json = JsonConvert.SerializeObject(command);
+
+            StringContent content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(ProductsRest.ProductsPath, content);
+
+            response.StatusCode.Should().Be(HttpStatusCode.Created);
+        }
+
+        public void Dispose()
+        {
+            using (ServiceProvider serviceProvider = ServicesFixture.ServiceCollection.BuildServiceProvider())
+            {
+                var productDbContext = serviceProvider.GetRequiredService<ProductsDbContext>();
+                productDbContext.Products.RemoveRange(productDbContext.Products.ToList());
+                productDbContext.Categories.RemoveRange(productDbContext.Categories.ToList());
+                productDbContext.SaveChanges();
+            }
         }
     }
 }
